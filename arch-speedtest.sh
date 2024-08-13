ca16b6d868d1279ced5b2ee70622eee6becc2f81
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2024-08-13 15:24:16 -0500

    update for latest instead of the 10 year old iso

--- arch-speedtest.sh
+++ arch-speedtest.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 
-isodate='2014.12.01'
 country='US'
 
-echo Using Arch Linux ISO $isodate
+echo Using Arch Linux ISO latest
 
 if [[ -f mirrorlist && -s mirrorlist ]]; then
     echo Reusing previously downloaded mirrorlist
@@ -11,21 +10,12 @@ if [[ -f mirrorlist && -s mirrorlist ]]; then
 else
     echo Downloading "$country" mirrorlist
     curl --progress-bar \
-        https://www.archlinux.org/mirrorlist/?country=$country > mirrorlist
+        https://archlinux.org/mirrorlist/?country=$country&protocol=http&protocol=https&ip_version=4&ip_version=6 > mirrorlist
 
-    sed \
-        -e '1,/United States/d' \
-        -e 's/#Server = //' \
-        -e "s:\$repo/os/\$arch:iso/$isodate/archlinux-$isodate-dual.iso:" \
-        -i mirrorlist
+    sed -e '1,/United States/d' -e 's/#Server = //' -e "s:\$repo/os/\$arch:iso/latest/archlinux-x86_64.iso :" -i mirrorlist
 fi
 
 echo -e "\nStart ISO download"
 
 # split = total concurrent connections
-aria2c \
-    --split=30 \
-    --max-connection-per-server=2 \
-    --max-tries=0 \
-    --lowest-speed-limit=200K \
-    $(cat mirrorlist | tr '\n' ' ')
+aria2c -i mirrorlist -s 30 --connect-timeout 15 -t 30 --uri-selector=feedback --allow-overwrite true --human-readable true --lowest-speed-limit=200K --max-tries=0
